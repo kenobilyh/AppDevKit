51cf3ef1a2c10448ff35724722d801f428fe7d8b
Jeff Lin <yhlin@yahooinc.com>
2023-02-16 23:02:01 +0800

    AppDevKitSPMDemo for verify SPM

---
+++ AppDevKitSPMDemo/AppDevKitSPMDemo/AppDevKitSPMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevKitSPMDemoApp.swift
+//  AppDevKitSPMDemo
+//
+//  Created by Jeff Lin on 2023/2/16.
+//
+
+import SwiftUI
+
+@main
+struct AppDevKitSPMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
